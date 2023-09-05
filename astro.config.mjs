21bd3cea72064e7a07c692f74022b64f990dd579
Jonas Geschonke <jonas.geschonke@gmx.de>
2023-09-05 22:54:53 +0200

    something... atleast a start

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://JonasGeschonke.github.io',
-    base: '/'
+  site: 'https://JonasGeschonke.github.io',
+  base: '/',
+  integrations: [mdx(), tailwind()]
 });
